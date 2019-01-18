4a108f0acf45834e5bd0389b34b3a278c59df422
Your Name <you@example.com>
2019-01-18 15:51:03 -0800

    Adding GET request file for k6

---
+++ db/k6/GET.js
@@ -0,0 +1,10 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+const randomNumber = () => {
+  return Math.round(Math.random() * 10000000);
+}
+
+export default function () {
+  http.get(`http://localhost:3050/homes/${randomNumber()}/suggestions`);
+};
