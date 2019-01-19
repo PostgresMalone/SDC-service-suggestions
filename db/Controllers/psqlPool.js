a2d2f868b60a64fc8af6823345276bc5577ae6be
Your Name <you@example.com>
2019-01-19 11:48:29 -0800

    Removed comments

--- db/Controllers/psqlPool.js
+++ db/Controllers/psqlPool.js
@@ -22,35 +22,35 @@ const getSuggestions = (id, callback) => {
   });
 }
 
-// const addSuggestions = (suggestion_id, city, country, beds, relation_id, thumbnail_img, price, states) => {
-//   const searchQuery = `INSERT INTO suggestions (suggestion_id, city, country, home_beds, 
-//   home_relation_id, home_thumbnail_img, home_price, states) 
-//   VALUES (${suggestion_id}, '${city}', '${country}', '${beds}','${relation_id}', '${thumbnail_img}', ${price}, '${states}')`;
+const addSuggestions = (suggestion_id, city, country, beds, relation_id, thumbnail_img, price, states) => {
+  const searchQuery = `INSERT INTO suggestions (suggestion_id, city, country, home_beds, 
+  home_relation_id, home_thumbnail_img, home_price, states) 
+  VALUES (${suggestion_id}, '${city}', '${country}', '${beds}','${relation_id}', '${thumbnail_img}', ${price}, '${states}')`;
 
-//   pool.query(searchQuery, (err, res) => {
-//     if (err) {
-//       console.log(err, 'error')
-//     } else {
-//       console.log('Succesfully added');
-//     }
-//   });
-// }
+  pool.query(searchQuery, (err, res) => {
+    if (err) {
+      console.log(err, 'error')
+    } else {
+      console.log('Succesfully added');
+    }
+  });
+}
 
-// const modifySuggestions = (column, value, id) => {
-//   value = typeof value === 'string' ? `'${value}'` : value;
-//   const searchQuery = `UPDATE suggestions SET ${column}=${value} WHERE suggestion_id=${id}`;
+const modifySuggestions = (column, value, id) => {
+  value = typeof value === 'string' ? `'${value}'` : value;
+  const searchQuery = `UPDATE suggestions SET ${column}=${value} WHERE suggestion_id=${id}`;
 
-//   pool.query(searchQuery, (err, res) => {
-//     if (err) {
-//       console.log(err, 'error modifying suggestion');
-//     } else {
-//       console.log('Succesfully modified');
-//     }
-//   })
-// }
+  pool.query(searchQuery, (err, res) => {
+    if (err) {
+      console.log(err, 'error modifying suggestion');
+    } else {
+      console.log('Succesfully modified');
+    }
+  })
+}
 
 module.exports = {
   getSuggestions,
-  // addSuggestions,
-  // modifySuggestions
+  addSuggestions,
+  modifySuggestions
 }
