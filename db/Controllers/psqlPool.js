01429a2a974923db2a20424f1470f4513c989561
Your Name <you@example.com>
2019-01-18 22:08:46 -0800

    Fixed connection problems stemming from opening new connection per query

--- db/Controllers/psqlPool.js
+++ db/Controllers/psqlPool.js
@@ -2,63 +2,50 @@ const pg = require('pg')
 const config = require('./config.js');
 
 const pool = new pg.Pool(config);
+pool.connect()
 
 pool.on('error', (err) => {
   console.log('Error on database', err);
 });
 
 const getSuggestions = (id, callback) => {
-  const searchQuery = `SELECT * FROM suggestions JOIN homes ON homes.home_id=suggestions.home_relation_id WHERE suggestions.home_relation_id=${id};`;
+  const searchQuery = `SELECT * FROM suggestions JOIN homes ON homes.
+  home_id=suggestions.home_relation_id WHERE suggestions.home_relation_id=${id};`;
 
-  pool.connect()
-    .then(client => {
-      return client.query(searchQuery)
-        .then(res => {
-          client.release();
-          callback(res.rows);
-        })
-        .catch(err => {
-          client.release();
-          console.log(err, 'error in getSuggestions')
-        });
-    });
+  pool.query(searchQuery, (err, res) => {
+    if (err) {
+      console.log(err, 'error')
+    } else {
+      callback(null, res.rows);
+    }
+  });
 }
 
 const addSuggestions = (suggestion_id, city, country, beds, relation_id, thumbnail_img, price, states) => {
+  const searchQuery = `INSERT INTO suggestions (suggestion_id, city, country, home_beds, 
+  home_relation_id, home_thumbnail_img, home_price, states) 
+  VALUES (${suggestion_id}, '${city}', '${country}', '${beds}','${relation_id}', '${thumbnail_img}', ${price}, '${states}')`;
 
-  let searchQuery = `INSERT INTO suggestions (suggestion_id, city, country, home_beds, home_relation_id, home_thumbnail_img, home_price, states) VALUES (${suggestion_id}, '${city}', '${country}', '${beds}','${relation_id}', '${thumbnail_img}', ${price}, '${states}')`;
-
-  pool.connect()
-    .then(client => {
-      return client.query(searchQuery)
-        .then(() => {
-          client.release();
-          console.log('Successfully saved')
-        })
-        .catch(err => {
-          client.release();
-          console.log(err, 'Error saving ')
-        });
-    });
+  pool.query(searchQuery, (err, res) => {
+    if (err) {
+      console.log(err, 'error')
+    } else {
+      console.log('Succesfully added');
+    }
+  });
 }
 
 const modifySuggestions = (column, value, id) => {
   value = typeof value === 'string' ? `'${value}'` : value;
+  const searchQuery = `UPDATE suggestions SET ${column}=${value} WHERE suggestion_id=${id}`;
 
-  let searchQuery = `UPDATE suggestions SET ${column}=${value} WHERE suggestion_id=${id}`;
-
-  pool.connect()
-    .then(client => {
-      return client.query(searchQuery)
-        .then(() => {
-          client.release();
-          console.log('Successfully modified')
-        })
-        .catch(err => {
-          client.release();
-          console.log(err, 'Error modifying');
-        });
-    });
+  pool.query(searchQuery, (err, res) => {
+    if (err) {
+      console.log(err, 'error modifying suggestion');
+    } else {
+      console.log('Succesfully modified');
+    }
+  })
 }
 
 module.exports = {
