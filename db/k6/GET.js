af7177d763dcbf9833ee18c83960d0b8786493ad
Your Name <you@example.com>
2019-01-18 21:54:20 -0800

    Adding k6 GET request module

--- db/k6/GET.js
+++ db/k6/GET.js
@@ -2,8 +2,13 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 const randomNumber = () => {
-  return Math.round(Math.random() * 10000000);
+  return Math.round(Math.random() * 1000000);
 }
+
+// const options = {
+//   vus: 100,
+//   duration: '60s'
+// }
 
 export default function () {
   http.get(`http://localhost:3050/homes/${randomNumber()}/suggestions`);
