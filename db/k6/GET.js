66cad7b5312d5033706c6c33b62280d07f38234d
Your Name <you@example.com>
2019-01-21 14:31:26 -0800

    Updated GET file to apply 70/30 rule

--- db/k6/GET.js
+++ db/k6/GET.js
@@ -1,10 +1,22 @@
 import http from "k6/http";
 import { sleep } from "k6";
 
-const randomNumber = () => {
-  return Math.round(Math.random() * 1000000);
+const generateNumberBetweenRange = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomNumber = (n) => {
+  return Math.round(Math.random() * n)
 }
 
 export default function () {
-  http.get(`http://localhost:3050/homes/${randomNumber()}/suggestions`);
+  const rangeToQuery = generateNumberBetweenRange(1, 10);
+
+  if (rangeToQuery <= 7) {
+    http.get(`http://localhost:3050/homes/${randomNumber(7000000)}/suggestions`)
+  } else {
+    http.get(`http://localhost:3050/homes/${generateNumberBetweenRange(7000001, 10000000)}/suggestions`)
+  }
 };
